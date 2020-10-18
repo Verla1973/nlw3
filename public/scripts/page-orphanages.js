f19a49699d548fa1c68f942fd180170cfdf08912
Eskurinho Dev <manoads2019@gmail.com>
2020-10-18 00:07:53 -0300

    workshop2.3

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,9 +1,27 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//create map
+const map = L.map('mapid').setView([-29.7717502,-57.093433], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>')
+
+
+L.marker([-29.7717502,-57.093433], { icon })
+.addTo(map)
+    .bindPopup(popup)
